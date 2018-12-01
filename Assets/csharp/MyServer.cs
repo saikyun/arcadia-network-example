8da186710cc9e624a651bc4e2eae564ab213c185
Jona <saikyun@gmail.com>
2018-12-02 00:16:36 +0100

    it's now possible to transfer data from both the server and from clients

--- Assets/csharp/MyServer.cs
+++ Assets/csharp/MyServer.cs
@@ -5,19 +5,29 @@ using clojure.lang;
 public class MyServer : NetworkManager
 {
   public static IFn gotEdnFn;
-
+  public static IFn sendEdnToAllFn;
+  
   public void Start() {
-    gotEdnFn = RT.var("game.core", "got-edn");
+    gotEdnFn = RT.var("game.core", "got-edn");    
+    sendEdnToAllFn = RT.var("game.core", "send-edn-to-all");    
+  }
+  
+  // This is needed when the VM restarts
+  public void Update() {
+    if (gotEdnFn == null) { gotEdnFn = RT.var("game.core", "got-edn"); }
+    if (sendEdnToAllFn == null) { sendEdnToAllFn = RT.var("game.core", "send-resp"); }
   }
   
   public override void OnStartServer()
   {
+    Debug.Log("starting server...");
     NetworkServer.RegisterHandler(EdnMsg.MsgType, OnServerGotEdn);
     base.OnStartServer();
   }
   
   public override void OnStartClient(NetworkClient client)
   {
+    Debug.Log("starting client...");
     client.RegisterHandler(EdnMsg.MsgType, OnClientGotEdn);
     base.OnStartClient(client);
   }
@@ -25,16 +35,17 @@ public class MyServer : NetworkManager
   public void OnServerGotEdn(NetworkMessage netMsg)  
   {  
     var msg = netMsg.ReadMessage<EdnMsg>();  
-    Debug.Log("Server received message");  
+    Debug.Log("Server received message from client " + netMsg.conn.connectionId);  
     Debug.Log(msg.edn);
     gotEdnFn.invoke(msg.edn);
-  } 
+    sendEdnToAllFn.invoke("to server, a message came from " + netMsg.conn.connectionId);
+  }
   
   public void OnClientGotEdn(NetworkMessage netMsg)  
   {  
-    var msg = netMsg.ReadMessage<EdnMsg>();  
-    Debug.Log("Client received message");  
-    Debug.Log(msg.edn);  
+    var msg = netMsg.ReadMessage<EdnMsg>();
+    Debug.Log("Client received message");
+    Debug.Log("msg: " + msg.edn);
     gotEdnFn.invoke(msg.edn);
   }
 }
--- Assets/game/core.clj
+++ Assets/game/core.clj
@@ -1,16 +1,58 @@
 (ns game.core
-  (:require [arcadia.core :refer :all])
-  (:import [UnityEngine.Networking NetworkServer]
+  (:require [arcadia.core :refer :all]
+            [clojure.core.server :as s]
+            [clojure.main :as m])
+  (:import [UnityEngine.Networking NetworkManager NetworkClient NetworkServer]
+           [UnityEngine Application]
            EdnMsg))
+
+;; Uncomment this when building if you want a repl in the standalone game
+;; Don't forget to comment it again before hitting "play" though
+;; (s/start-server {:port 6015 :name "running-server" :accept `m/repl})
+
+;; The ugly def is to make it easier to do stuff when you connect to the
+;;   standalone games repl
+;; This has to do with certain functions not being okay to run
+;;   unless you're in the main thread. I couldn't get it to work so...
+(defn client []
+  (let [c (.. (cmpt (object-named "NetworkManager") NetworkManager) client)]
+    (when c
+      (def client-holder c))
+    c))
+
+;; If you wanna call this from the standalone repl, you can do something like
+;;  (send-edn-to-server client-holder {:cool-data "yeah"})
+(defn send-edn-to-server
+  [client edn]
+  (.. client (Send (.. EdnMsg MsgType) (EdnMsg. (prn-str edn)))))
+
+(defn on-button-click []
+  (send-edn-to-server (client) {:stuff "hej"}))
+
+(defrole frame-rate-role
+  (start [obj k]
+         (set! (.. Application targetFrameRate) 5)))
 
 (defonce data (atom {}))
 
-(add-watch data :data-watcher (fn [_ _ _ _] (log "got new data! " data)))
+(add-watch data :data-watcher (fn [_ _ _ _] (log "got new data! " @data)))
+
+(defn is-server? [] (.. NetworkServer active))
+(defn is-client? [] (.. NetworkClient active))
 
 (defn got-edn
   [edn]
   (reset! data (read-string edn)))
 
-(defn send-edn
+(defn send-edn-to-all
   [edn]
-  (.. NetworkServer (SendToAll (.. EdnMsg MsgType) (EdnMsg. edn))))
+  (.. NetworkServer (SendToAll (.. EdnMsg MsgType) (EdnMsg. (prn-str edn)))))
+
+(defn send-resp
+  [msg]
+  (.. NetworkServer (SendToAll (.. EdnMsg MsgType) (EdnMsg. (prn-str msg)))))
+
+(defrole button-role
+  (on-pointer-click
+   [obj click-data k]
+   (on-button-click)))
